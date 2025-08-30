b398cc7fca73c368bd78451bdc5cbbc7a35eb132
Ilja Goushcha <iljagoushcha@Mac.attlocal.net>
2025-08-30 16:14:16 -0700

    Initial commit

---
+++ src/main/java/com/myprojects/pauldirac/PauldiracApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.pauldirac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PauldiracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PauldiracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojects/pauldirac/PauldiracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.pauldirac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauldiracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
