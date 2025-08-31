8e6741c86289494676c4d951bb25187a08fa5417
Ilja Goushcha <iljagoushcha@Mac.attlocal.net>
2025-08-30 17:48:14 -0700

    testing branch push

--- src/main/java/com/myprojects/pauldirac/controller/FunRestController.java
+++ src/main/java/com/myprojects/pauldirac/controller/FunRestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
+    // testing branch commit
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
