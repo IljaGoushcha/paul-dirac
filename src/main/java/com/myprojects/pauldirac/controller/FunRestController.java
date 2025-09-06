e4ff668584d8202e94db29028cacaeae0a374427
Ilja Goushcha <iljagoushcha@Mac.attlocal.net>
2025-09-06 09:00:15 -0700

    setting up actuator

--- src/main/java/com/myprojects/pauldirac/controller/FunRestController.java
+++ src/main/java/com/myprojects/pauldirac/controller/FunRestController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-    // testing branch commit
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
     }
 
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run hard 5k!!!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
 }
