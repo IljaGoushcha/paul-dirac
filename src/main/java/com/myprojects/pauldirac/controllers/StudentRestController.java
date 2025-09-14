56fb2f0aeec0d8f41668829b70721bb7755e3d88
Ilja Goushcha <iljagoushcha@Mac.attlocal.net>
2025-09-14 10:52:00 -0700

    working on exception handling

--- src/main/java/com/myprojects/pauldirac/controllers/StudentRestController.java
+++ src/main/java/com/myprojects/pauldirac/controllers/StudentRestController.java
@@ -3,10 +3,9 @@ package com.myprojects.pauldirac.controllers;
 import com.myprojects.pauldirac.dao.StudentDAOImpl;
 import com.myprojects.pauldirac.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +14,12 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @Autowired
     StudentDAOImpl studentDAOImpl;
+
+    @Autowired
+    public StudentRestController(StudentDAOImpl studentDAOImpl) {
+        this.studentDAOImpl = studentDAOImpl;
+    }
 
     @GetMapping("/students")
     public List<Student> getListOfStudents() {
@@ -30,6 +33,10 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudentById(@PathVariable int studentId) {
         Student response = studentDAOImpl.findById(studentId);
+        System.out.println("response: " + response);
+        if (response == null ) {
+            throw new StudentNotFoundException("student with id=" + studentId + " does not exist.");
+        }
         return response;
     }
 
